1b5e44efeeceb72d5f6e72fb65498e32f6a7a19a
Huzaifa-890 <huzaifa890@gmail.com>
2024-12-18 22:07:43 +0500

    pull

--- src/screens/Auth/Welcome.jsx
+++ src/screens/Auth/Welcome.jsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native';
+import { View, Text,TouchableOpacity  } from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';  
 
 const Welcome = () => {
   return (
     <View>
       <Text>Welcome Screen</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity >
+        <Icon name="home" size={30} color="#000" />
+      </TouchableOpacity>
+    </View>
     </View>
   );
 };
